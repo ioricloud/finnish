94ff7331d632a0ec34ec74d0e902dec407c14317
Henrique Oliveira <ioricloud@gmail.com>
2011-08-04 16:53:15 -0300

    terceiro commit

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -16,4 +16,44 @@ class SiteController < ApplicationController
 	
 	def contato
 	end
+	
+	def horas
+	end
+	
+	def pronomes
+	end
+	
+	def onde
+	end
+	
+	def verboter
+	end
+	
+	def tipoverbo
+	end
+	
+	def conjugacoes
+	end
+	
+	def partitivo
+	end
+	
+	def negativo
+	end
+	
+	def interrogativo
+	end
+	
+	def verboserter
+	end
+	
+	def participio
+	end
+	
+	def confidicional
+	end
+	
+	def imperfeito
+	end
+	
 end
