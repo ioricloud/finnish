9ce498382281a58b606bcd8d2c63ff88c0353beb
Henrique Oliveira <ioricloud@gmail.com>
2011-08-04 11:49:27 -0300

    primeiro commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,19 @@
+class SiteController < ApplicationController
+	def index
+	end
+	
+	def finlandia
+	end
+	
+	def abc
+	end
+	
+	def pronuncia
+	end
+	
+	def numeros
+	end
+	
+	def contato
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finnish::Application.config.secret_token = '7d7b213ba46251202d0a65116fe6f0686dbf15ca0ce45a4f903ee924fe4836578f022f303861a3616ac1a48e81f51a5e4612406fbe004c4ecaf9857c65625498'
